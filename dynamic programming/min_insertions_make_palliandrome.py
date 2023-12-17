8a5bbf30c8bd5c214de3a8ce475704d2f9aa8c7e
Shobhit Gupta <shobhit.gupta1995@gmail.com>
2023-12-17 12:09:39 +0530

    leetcode 1312

---
+++ dynamic programming/min_insertions_make_palliandrome.py
@@ -0,0 +1,22 @@
+# Leetcode 1312. Minimum Insertion Steps to Make a String Palindrome
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        s1 = s
+        s2 = s1[::-1] # reverse string
+        m,n = len(s1), len(s2)
+        # dp table initialization
+        t = [[0 for j in range(n+1)] for i in range(m+1)]
+
+        # dp table calculating
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s1[i-1] == s2[j-1]:
+                    t[i][j] = 1 + t[i-1][j-1]
+                else:
+                    t[i][j] = max(t[i-1][j], t[i][j-1])
+        
+
+        return m - t[m][n]
+    
+
+print(Solution().minInsertions('mbadm'))
