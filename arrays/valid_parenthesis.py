f643ef5b8e85586ddde6f78cf5a62bbccc158367
Shobhit Gupta <shobhit.gupta1995@gmail.com>
2023-12-23 21:17:41 +0530

    Added valid parenthesis

---
+++ arrays/valid_parenthesis.py
@@ -0,0 +1,24 @@
+# Leetcode 30
+# Given a string containing parenthesis, find if they are valid
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        if len(s)%2 == 1:
+            return False
+
+        closing = {'}',')',']'}
+        pair = {')':'(', ']':'[', '}':'{'}
+
+        stack = []
+
+        for char in s:
+            if stack and char in closing and stack[-1] == pair[char]:
+                stack.pop()
+            else:
+                stack.append(char)
+        if len(stack) == 0:
+            return True
+        return False
+    
+print(Solution().isValid("[({})]{[]}"));
+print(Solution().isValid("}{"))
