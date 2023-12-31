6755a66960dc880019ca14b1d9025dfae11630a1
Shobhit Gupta <shobhit.gupta1995@gmail.com>
2023-12-31 18:01:44 +0530

    /usr/bin/python3

---
+++ tree/max_path_sum.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# Leetcode 124
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxPathSum(self, root) -> int:
+
+        res = float('-inf')
+
+        def dfs(node):
+            nonlocal res
+
+            if not node:
+                return 0
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            temp = max(node.val, node.val + max(left, right))
+
+            ans = max(temp, node.val + left + right)
+
+            res = max(res, ans)
+
+            return temp
+
+        dfs(root)
+
+        return res
+        
+left = TreeNode(2)
+right = TreeNode(3)
+
+treeRoot = TreeNode(val = 1, left=left, right=right)
+
+print(Solution().maxPathSum(treeRoot))
